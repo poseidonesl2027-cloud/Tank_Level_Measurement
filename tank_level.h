0e68f3bf3ab2c5587b8913195c5bcc911fe459ad
poseidonesl2027-cloud <poseidonesl2027@gmail.com>
2026-04-16 15:12:30 +0100

    added header file

---
+++ tank_level.h
@@ -0,0 +1,30 @@
+
+#ifndef _TANK_LEVEL_H_
+#define _TANK_LEVEL_H_
+
+/******************************************************************************
+ * Poseidon Embedded Software Ltd
+ *
+ * @file    tank_level.h
+ * @brief   <public interface description>
+ * @author  Poseidon Embedded Software Ltd
+ * @date    16 Apr 2026
+ *
+ * MIT License Copyright (c) 2026 Poseidon Embedded Software Ltd
+ ******************************************************************************/
+
+/* Includes -----------------------------------------------------------------*/
+
+/* Macros -------------------------------------------------------------------*/
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#define MEDIAN_SIZE 5
+#define MA_WINDOW_SIZE 8
+
+/* Types --------------------------------------------------------------------*/
+
+/* Public Function Prototypes -----------------------------------------------*/
+
+#endif /* <MODULE_NAME>_H */
